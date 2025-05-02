1fc063beaa2d9b9b4b0c67aebf2247dcf0f0ce3d
maximiliancsuk <maximilian.csuk@max-bytes.com>
2025-05-02 13:09:49 +0200

    bugfix

--- app/Bots.js
+++ app/Bots.js
@@ -553,4 +553,6 @@ function* act_tc_jus_base_1(message, state, response, responseSystem, parting1, 
     }
 }
 
-export const act_tc_jus_1Moot_BR_e1 = act_tc_jus_base_1.bind(undefined, undefined, undefined, undefined, "Dann schau mal rüber zum Elektronischen Kommunikationsterminal im Moot Court Room (RESOWI, Bauteil B, Erdgeschloss)", "Aber achte UNBEDINGT auf die richtige Seriennummer: #11689");
+export function* act_tc_jus_1Moot_BR_e1(message, state, response, responseSystem) {
+    yield* act_tc_jus_base_1.bind(message, state, response, responseSystem, "Dann schau mal rüber zum Elektronischen Kommunikationsterminal im Moot Court Room (RESOWI, Bauteil B, Erdgeschloss)", "Aber achte UNBEDINGT auf die richtige Seriennummer: #11689");
+}
